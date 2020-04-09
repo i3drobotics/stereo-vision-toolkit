adced3e1f5ee300218588853bfc6b36047df9045
bknight-i3drobotics <bknight@i3drobotics.com>
2020-04-09 22:25:57 +0100

    add qt to 3rd_party

---
+++ 3rd_party/qt5.10.1/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=C:\Qt\Qt5.10.1\5.10.1\msvc2015_64\bin;%PATH%
+cd /D C:\Qt\Qt5.10.1\5.10.1\msvc2015_64
+echo Remember to call vcvarsall.bat to complete environment setup!
