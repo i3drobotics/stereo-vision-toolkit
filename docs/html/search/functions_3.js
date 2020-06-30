28138de4cf219c3a3a4297de9e7b4b31ebcf4e55
Ben Knight <benknight135@gmail.com>
2020-06-30 17:53:43 +0100

    update docs

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['disconnected_305',['disconnected',['../class_abstract_stereo_camera.html#a3d09b66be2f4c53280d4328ff5409881',1,'AbstractStereoCamera']]],
-  ['disparitysavecheckchanged_306',['disparitySaveCheckChanged',['../class_disparity_viewer.html#a9c5419642d512bb184fc7abd73531d71',1,'DisparityViewer']]],
-  ['downsampleimage_307',['downsampleImage',['../class_calibration_dialog.html#a2a2cd870cf2a3be514811047351b5816',1,'CalibrationDialog']]]
+  ['disconnected_306',['disconnected',['../class_abstract_stereo_camera.html#a3d09b66be2f4c53280d4328ff5409881',1,'AbstractStereoCamera']]],
+  ['disparitysavecheckchanged_307',['disparitySaveCheckChanged',['../class_disparity_viewer.html#a9c5419642d512bb184fc7abd73531d71',1,'DisparityViewer']]],
+  ['downsampleimage_308',['downsampleImage',['../class_calibration_dialog.html#a2a2cd870cf2a3be514811047351b5816',1,'CalibrationDialog']]]
 ];
--- docs/html/search/functions_c.js
+++ docs/html/search/functions_c.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['readfileraw_394',['ReadFileRaw',['../class_i3_d_r_s_g_m.html#a83e267239981e7e6a069cae334c5c5de',1,'I3DRSGM']]],
-  ['rectified_395',['rectified',['../class_abstract_stereo_camera.html#ac86037594fcc19b765cefce6f240e2c2',1,'AbstractStereoCamera']]],
-  ['rectifyimages_396',['rectifyImages',['../class_abstract_stereo_camera.html#a87141c00b70c9dd12b1a7a6b01610b29',1,'AbstractStereoCamera']]],
-  ['remap_5fparallel_397',['remap_parallel',['../class_abstract_stereo_camera.html#a6c0c5ae0ed8fad72e65b4132f2b40f29',1,'AbstractStereoCamera']]],
-  ['reproject3d_398',['reproject3D',['../class_abstract_stereo_camera.html#a63e7670737b9e6ee4f5c6d9daf0940c4',1,'AbstractStereoCamera']]],
-  ['reprojected_399',['reprojected',['../class_abstract_stereo_camera.html#a955561b40fdaeaf218e32fb117b6aa9b',1,'AbstractStereoCamera']]],
-  ['run_5fcalibration_400',['run_calibration',['../class_calibrate_from_images_dialog.html#a1733440fa386d1482cbf7dc2704c2612',1,'CalibrateFromImagesDialog']]]
+  ['readfileraw_395',['ReadFileRaw',['../class_i3_d_r_s_g_m.html#a83e267239981e7e6a069cae334c5c5de',1,'I3DRSGM']]],
+  ['rectified_396',['rectified',['../class_abstract_stereo_camera.html#ac86037594fcc19b765cefce6f240e2c2',1,'AbstractStereoCamera']]],
+  ['rectifyimages_397',['rectifyImages',['../class_abstract_stereo_camera.html#a87141c00b70c9dd12b1a7a6b01610b29',1,'AbstractStereoCamera']]],
+  ['remap_5fparallel_398',['remap_parallel',['../class_abstract_stereo_camera.html#a6c0c5ae0ed8fad72e65b4132f2b40f29',1,'AbstractStereoCamera']]],
+  ['reproject3d_399',['reproject3D',['../class_abstract_stereo_camera.html#a63e7670737b9e6ee4f5c6d9daf0940c4',1,'AbstractStereoCamera']]],
+  ['reprojected_400',['reprojected',['../class_abstract_stereo_camera.html#a955561b40fdaeaf218e32fb117b6aa9b',1,'AbstractStereoCamera']]],
+  ['run_5fcalibration_401',['run_calibration',['../class_calibrate_from_images_dialog.html#a1733440fa386d1482cbf7dc2704c2612',1,'CalibrateFromImagesDialog']]]
 ];
