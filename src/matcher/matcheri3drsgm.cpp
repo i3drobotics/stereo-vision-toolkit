1c4648dd3c2ad2f50fb8fb5b9cf2665cdbe7f88b
Ben Knight <benknight135@gmail.com>
2020-06-30 13:17:58 +0100

    rename i3drsgm class

--- src/matcher/matcheri3drsgm.cpp
+++ src/matcher/matcheri3drsgm.cpp
@@ -1,7 +1,7 @@
-#include "qmatcheri3drsgm.h"
+#include "matcheri3drsgm.h"
 
 //Initialise matcher
-void QMatcherI3DRSGM::init()
+void MatcherI3DRSGM::init()
 {
     QString qparam_file = QCoreApplication::applicationDirPath() + "/i3drsgm.param";
     std::string param_file = qparam_file.toStdString();
@@ -9,26 +9,26 @@ void QMatcherI3DRSGM::init()
     QString qtmp_param_file = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/tmp.param";
     std::string tmp_param_file = qtmp_param_file.toStdString();
 
-    i3drsgm = new MatcherI3DRSGM(tmp_param_file,param_file);
+    i3drsgm = new I3DRSGM(tmp_param_file,param_file);
 }
 
-int QMatcherI3DRSGM::getStatus()
+int MatcherI3DRSGM::getStatus()
 {
     return this->i3drsgm->getStatus();
 }
 
-bool QMatcherI3DRSGM::isLicenseValid()
+bool MatcherI3DRSGM::isLicenseValid()
 {
     return this->i3drsgm->isLicenseValid();
 }
 
-int QMatcherI3DRSGM::getErrorDisparity(void)
+int MatcherI3DRSGM::getErrorDisparity(void)
 {
     return this->i3drsgm->getErrorDisparity();
 }
 
 //compute disparity
-void QMatcherI3DRSGM::forwardMatch()
+void MatcherI3DRSGM::forwardMatch()
 {
     cv::Mat oDisparity;
     oDisparity = this->i3drsgm->forwardMatch(*left,*right);
@@ -36,94 +36,94 @@ void QMatcherI3DRSGM::forwardMatch()
 }
 
 //backward match disparity
-void QMatcherI3DRSGM::backwardMatch()
+void MatcherI3DRSGM::backwardMatch()
 {
     cv::Mat oDisparity;
     oDisparity = this->i3drsgm->backwardMatch(*left,*right);
     oDisparity.convertTo(disparity_rl, CV_32F, -16);
 }
 
-void QMatcherI3DRSGM::enableCPU(bool enable)
+void MatcherI3DRSGM::enableCPU(bool enable)
 {
     this->i3drsgm->enableCPU(enable);
 }
 
-void QMatcherI3DRSGM::setNoDataValue(int val)
+void MatcherI3DRSGM::setDisparityError(int val)
 {
-    this->i3drsgm->setNoDataValue(val);
+    this->i3drsgm->setDisparityError(val);
 }
 
-void QMatcherI3DRSGM::setSpeckleDifference(float diff)
+void MatcherI3DRSGM::setSpeckleDifference(float diff)
 {
   this->i3drsgm->setSpeckleDifference(diff);
 }
 
-void QMatcherI3DRSGM::setSpeckleSize(int size)
+void MatcherI3DRSGM::setSpeckleSize(int size)
 {
    this->i3drsgm->setSpeckleSize(size);
 }
 
-void QMatcherI3DRSGM::setMatchCosts(float P1, float P2){
+void MatcherI3DRSGM::setMatchCosts(float P1, float P2){
     setP1(P1);
     setP2(P2);
 }
 
-void QMatcherI3DRSGM::setP1(float P1)
+void MatcherI3DRSGM::setP1(float P1)
 {
    this->i3drsgm->setP1(P1);
 }
 
-void QMatcherI3DRSGM::setP2(float P2)
+void MatcherI3DRSGM::setP2(float P2)
 {
    this->i3drsgm->setP2(P2);
 }
 
-void QMatcherI3DRSGM::setWindowSize(int census_size)
+void MatcherI3DRSGM::setWindowSize(int census_size)
 {
    this->i3drsgm->setWindowSize(census_size);
 }
 
-void QMatcherI3DRSGM::setDisparityShift(int shift)
+void MatcherI3DRSGM::setDisparityShift(int shift)
 {
     this->i3drsgm->setDisparityShift(shift);
 }
 
-void QMatcherI3DRSGM::maxPyramid(int pyramid_num)
+void MatcherI3DRSGM::maxPyramid(int pyramid_num)
 {
    this->i3drsgm->maxPyramid(pyramid_num);
 }
 
-void QMatcherI3DRSGM::enablePyramid(bool enable, int pyramid_num)
+void MatcherI3DRSGM::enablePyramid(bool enable, int pyramid_num)
 {
     this->i3drsgm->enablePyramid(enable, pyramid_num);
 }
 
-void QMatcherI3DRSGM::enableSubpixel(bool enable)
+void MatcherI3DRSGM::enableSubpixel(bool enable)
 {
     this->i3drsgm->enableSubpixel(enable);
 }
 
-void QMatcherI3DRSGM::setDisparityRange(int n)
+void MatcherI3DRSGM::setDisparityRange(int n)
 {
     this->i3drsgm->setDisparityRange(n);
 }
 
-void QMatcherI3DRSGM::enableTextureDSI(bool enable)
+void MatcherI3DRSGM::enableTextureDSI(bool enable)
 {
   this->i3drsgm->enableTextureDSI(enable);
 }
 
-void QMatcherI3DRSGM::enableInterpolation(bool enable)
+void MatcherI3DRSGM::enableInterpolation(bool enable)
 {
     this->i3drsgm->enableInterpolation(enable);
 }
 
-void QMatcherI3DRSGM::enableOcclusionDetection(bool enable)
+void MatcherI3DRSGM::enableOcclusionDetection(bool enable)
 {
     this->i3drsgm->enableOcclusionDetection(enable);
 }
 
-void QMatcherI3DRSGM::enableOccInterpol(bool enable)
+void MatcherI3DRSGM::enableOccInterpol(bool enable)
 {
     this->i3drsgm->enableOccInterpol(enable);
 }
