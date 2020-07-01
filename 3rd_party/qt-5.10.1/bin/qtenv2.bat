73471360947555338aa18a6c87468752ea130b74
Ben Knight <benknight135@gmail.com>
2020-07-01 16:45:42 +0100

    re-add changes after large file git issue

--- 3rd_party/qt-5.10.1/bin/qtenv2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Setting up environment for Qt usage...
-set PATH=C:\Qt\Qt5.10.1\5.10.1\msvc2015_64\bin;%PATH%
-cd /D C:\Qt\Qt5.10.1\5.10.1\msvc2015_64
-echo Remember to call vcvarsall.bat to complete environment setup!
--- 3rd_party/qt-5.10.1/include/QtBluetooth/5.10.1/QtBluetooth/private/qtbluetooth-config_p.h
+++
@@ -1,3 +0,0 @@
-#define QT_FEATURE_bluez_le -1
-#define QT_FEATURE_linux_crypto_api -1
-#define QT_FEATURE_winrt_bt 1
