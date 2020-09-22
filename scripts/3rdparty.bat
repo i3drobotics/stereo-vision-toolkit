51b6452d2a96ae11d3f6cd8a1e92eb6e9794af14
bknight-i3drobotics <bknight@i3drobotics.com>
2020-09-22 17:56:16 +0100

    update to i3drsgm v1.0.3

--- scripts/3rdparty.bat
+++ scripts/3rdparty.bat
@@ -34,8 +34,8 @@ if "%option_uninstall%"=="true" (
 )
 
 if "%option_with_i3drsgm%"=="true" (
-    :: install i3drsgm (also install phobos-integration dependency but not opencv)
-    call ..\3rdparty\i3drsgm-1.0.2\setup_i3drsgm.bat %script_params% --with-phobos-integration
+    :: install i3drsgm
+    call ..\3rdparty\i3drsgm-1.0.2\install_i3drsgm.bat %script_params%
 )
 
 :: install boost (vorboss mirror specified for downloading from sourceforge optimized for EU)
