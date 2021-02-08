5c6fc6ee000b2525905042f2c2e4fa8011e75172
Ben <benknight135@gmail.com>
2021-02-08 18:05:17 +0000

    Update camera control arduino script

--- src/camera/cameracontrol/cameracontrol/cameracontrol.ino
+++ src/camera/cameracontrol/cameracontrol/cameracontrol.ino
@@ -9,31 +9,20 @@
 #define PHOBOS_GIGE_TRIGGER_1 3
 #define PHOBOS_GIGE_TRIGGER_2 2
 
-#define TITANIA_USB_TRIGGER_1 4
-#define TITANIA_USB_TRIGGER_2 6
+#define TITANIA_USB_TRIGGER_1 1
+#define TITANIA_USB_TRIGGER_2 3
 
 //Choose trigger pins based on the camera being used
 #define CAMERA_TRIGGER_1 TITANIA_USB_TRIGGER_1 
 #define CAMERA_TRIGGER_2 TITANIA_USB_TRIGGER_2
 
-//Comment out when not using the IMU
+//Comment out line when not using the IMU
 #define USE_IMU
 #ifdef USE_IMU
-  #include <SoftwareSerial.h>
   #include <Wire.h>
   #include <Adafruit_Sensor.h>
   #include <Adafruit_BNO055.h>
   #include <utility/imumaths.h>
-  
-  #define TITANIA_USB_SS_RX 2
-  #define TITANIA_USB_SS_TX 3
-
-  //Choose software serial pins based on the camera being used
-  #define CAMERA_SS_RX TITANIA_USB_SS_RX 
-  #define CAMERA_SS_TX TITANIA_USB_SS_TX
-
-  //Setup software serial
-  SoftwareSerial softSerial(CAMERA_SS_RX, CAMERA_SS_TX);
 
   //Setup imu device
   Adafruit_BNO055 bno = Adafruit_BNO055(55);
@@ -43,63 +32,85 @@ double frame_delay;      // amount of time between triggered (1/fps)
 int trigger_time = 10;   // time for trigger to be registered by camera
 double fps = 5;          // inital fps
 String inString = "";    // string to hold input
+unsigned long lastTriggerTime = 0;
+unsigned long timeSinceTrigger;
+#ifdef USE_IMU
+  bool imuInit = false;
+#endif
 
 void setup() {
   Serial.begin(115200);
   #ifdef USE_IMU
-    softSerial.begin(9600);
-    /* Initialise the IMU sensor */
+    // Initialise the IMU sensor
     if(!bno.begin())
     {
-      /* There was a problem detecting the BNO055 ... check your connections */
-      Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+      // Problem detecting the BNO055 ... check your connections
+      // serial will return -1000 in all axis in loop to indicate the problem
+      imuInit = false;
     } else {
       bno.setExtCrystalUse(true);
+      imuInit = true;
     }
   #endif
   pinMode(CAMERA_TRIGGER_1, OUTPUT);
   pinMode(CAMERA_TRIGGER_2, OUTPUT);
   pinMode(LED_BUILTIN,OUTPUT);
+  
+  lastTriggerTime = millis();
 }
 
 void loop() {
+  // Check time since last trigger
+  timeSinceTrigger = millis() - lastTriggerTime;
+  // Calculate frame delay from fps (can change based on user input)
   frame_delay = 1000/fps;
-  
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(CAMERA_TRIGGER_1, HIGH);
-  digitalWrite(CAMERA_TRIGGER_2, HIGH);
-  delay(trigger_time);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(CAMERA_TRIGGER_1, LOW);
-  digitalWrite(CAMERA_TRIGGER_2, LOW);
-  delay(frame_delay-trigger_time);
 
+  // Wait for time to trigger the camera (without stopping the program)
+  if (timeSinceTrigger > frame_delay){
+    // Triggger camera (pulse high and then low
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(CAMERA_TRIGGER_1, HIGH);
+    digitalWrite(CAMERA_TRIGGER_2, HIGH);
+    lastTriggerTime = millis();
+    delay(trigger_time); // Wait small time for high to be registered by camera
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(CAMERA_TRIGGER_1, LOW);
+    digitalWrite(CAMERA_TRIGGER_2, LOW);
+  }
+  
   // Read trigger frame rate from serial
   if (Serial.available() > 0) { //Only read if data is available
     int inChar = Serial.read();
     if (isDigit(inChar)) {
-      // convert the incoming byte to a char and add it to the string:
+      // Convert the incoming byte to a char and add it to the string:
       inString += (char)inChar;
     }
-    // if you get a newline, print the string, then the string's value:
+    // If you get a newline, print the string, then the string's value:
     if (inChar == '\n') {
       fps = inString.toInt();
       // clear the string for new input:
       inString = "";
     }
-  }
-
   #ifdef USE_IMU
-    // Get a new IMU sensor event
-    sensors_event_t event; 
-    bno.getEvent(&event);
-    
-    // Display the floating point data
-    softSerial.print(event.orientation.x, 4);
-    softSerial.print(",");
-    softSerial.print(event.orientation.y, 4);
-    softSerial.print(",");
-    softSerial.print(event.orientation.z, 4);
-    softSerial.println();
+  } else {
+    if (imuInit){
+      // Get a new IMU sensor event
+      sensors_event_t event; 
+      bno.getEvent(&event);
+      
+      // Display the floating point data
+      Serial.print(event.orientation.x, 4);
+      Serial.print(",");
+      Serial.print(event.orientation.y, 4);
+      Serial.print(",");
+      Serial.print(event.orientation.z, 4);
+      Serial.println();
+    } else {
+      // IMU failed to initalise return -1000 in all axis
+      Serial.println("-1000,-1000,-1000");
+    }
+  }
+  #else
+  }
   #endif
 }
