ef6aeb995bac21896441c56beabeb96095b2623b
Ben Knight <benknight135@gmail.com>
2022-02-09 17:31:59 +0000

    assume jom is in path variable in build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -8,7 +8,6 @@ cd %scriptpath:~0,-1%
 cd ..
 
 set "visual_studio_path=%programfiles(x86)%\Microsoft Visual Studio\2019\Community"
-set "qt_creator_path=C:\Qt\Tools\QtCreator\bin"
 
 :: setup visual studio variables
 call "%visual_studio_path%\VC\Auxiliary\Build\vcvars64.bat"
@@ -17,10 +16,10 @@ mkdir build
 cd build
 :: release build
 qmake.exe "CONFIG+=qtquickcompiler WITH_I3DRSGM" ..\stereo_vision_toolkit.pro -spec win32-msvc
-%qt_creator_path%\jom.exe
+jom.exe
 :: debug build
 qmake.exe "CONFIG+=debug qml_debug qtquickcompiler" ..\stereo_vision_toolkit.pro -spec win32-msvc
-%qt_creator_path%\jom.exe
+jom.exe
 
 :: reset working directory
 cd %initcwd%
